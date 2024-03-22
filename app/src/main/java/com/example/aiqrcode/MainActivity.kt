8adecd116f5c50a20239242c2ab6224bbdd36711
Viktoriia Roi <victoriyaroi2003@gmail.com>
2024-03-22 21:32:57 +0200

    initial commit

---
+++ app/src/main/java/com/example/aiqrcode/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.aiqrcode
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.aiqrcode.ui.theme.AiQrCodeTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AiQrCodeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
